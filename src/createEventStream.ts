cd0269b72c4e377e6c4e642403144eae2747b7fd
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-11-29 18:27:06 +0700

    release: v.0.7.0-alpha.1

--- src/createEventStream.ts
+++ src/createEventStream.ts
@@ -1,10 +1,11 @@
-import { IEventStream } from "atomservicescore";
+import { EventStream, IEventStream } from "atomservicescore";
 import { Connector as RBMQConnector, IMQChannel } from "rbmq";
 import { endpoints } from "./endpoints";
 
 export const createEventStream = (configs?: { url: string; options?: any; }, factory?: any) => ((Configs): IEventStream => {
   let mqChannel: IMQChannel;
   const Factory = factory || RBMQConnector;
+  const StreamDefinitions: EventStream.IServiceStreamDefinition[] = [];
 
   const connector = Factory(Configs);
   const connect = async (): Promise<IMQChannel> => {
@@ -17,6 +18,101 @@ export const createEventStream = (configs?: { url: string; options?: any; }, fac
   };
 
   return {
+    connect: async () => {
+      const Channel = await connect();
+      const exchanges: string[] = [];
+      const queues: { [q: string]: { q: string; onMessage: any; }; } = {};
+      const binds: Array<{ ex: string; q: string; topic: string; }> = [];
+
+      for (const definition of StreamDefinitions) {
+        const { handlers, reactions, scope, type } = definition;
+
+        handlers.events.forEach(({ level, name }) => {
+          const ex = endpoints.toExchange({ level, scope, type });
+          const q = endpoints.toQueue({ channel: "EventHandler", scope, type });
+
+          if (exchanges.indexOf(ex) < 0) {
+            exchanges.push(ex);
+          }
+
+          if (queues[q] === undefined) {
+            const onMessage = Channel.toOnMessageWithAck((data, ack) => {
+              const { event, metadata } = data;
+              const processAck = async () => {
+                ack();
+              };
+              handlers.processing(event, metadata, processAck);
+            });
+
+            queues[q] = { q, onMessage };
+          }
+
+          binds.push({ ex, q, topic: name });
+        });
+
+        reactions.events.forEach((reaction) => {
+          const ex = endpoints.toExchange({ level: "Public", scope: reaction.scope, type: reaction.type });
+          const q = endpoints.toQueue({ channel: "EventReaction", scope, type });
+
+          if (exchanges.indexOf(ex) < 0) {
+            exchanges.push(ex);
+          }
+
+          if (queues[q] === undefined) {
+            const onMessage = Channel.toOnMessageWithAck((data, ack) => {
+              const { event, metadata } = data;
+              const processAck = async () => {
+                ack();
+              };
+              reactions.processes[reaction.scope](event, metadata, processAck);
+            });
+
+            queues[q] = { q, onMessage };
+          }
+        });
+
+        // 1. START :: assert Exchanges
+        const assertExchanges = [];
+
+        for (const ex of exchanges) {
+          assertExchanges.push(Channel.assertExchange(ex, "direct", { autoDelete: true, durable: true }));
+        }
+
+        await Promise.all(assertExchanges);
+        // END :: assert Exchanges
+
+        // 2. START :: assert Queues
+        const assertQueues = [];
+
+        for (const q of Object.keys(queues)) {
+          assertQueues.push(Channel.assertQueue(q, { autoDelete: true, durable: true }));
+        }
+
+        await Promise.all(assertExchanges);
+        // END :: assert Queues
+
+        // 3. START :: assert Binds
+        const assertBinds = [];
+
+        for (const { ex, q, topic } of binds) {
+          assertBinds.push(Channel.bindQueue(q, ex, topic));
+        }
+
+        await Promise.all(assertBinds);
+        // END :: assert Binds
+
+        // 4. START :: assert Consumes
+        const assertConsumes = [];
+
+        for (const key of Object.keys(queues)) {
+          const { q, onMessage } = queues[key];
+          assertConsumes.push(Channel.consume(q, onMessage, { noAck: false }));
+        }
+
+        await Promise.all(assertConsumes);
+        // END :: assert Consumes
+      }
+    },
     directTo: async (ref, data) => {
       const channel = await connect();
 
@@ -32,47 +128,20 @@ export const createEventStream = (configs?: { url: string; options?: any; }, fac
       await channel.assertQueue(q, { autoDelete: true, durable: true });
       await channel.consume(q, channel.toOnMessage(listeners), { noAck: true });
     },
-    publish: async (event, metadata, { level, scope }) => {
+    publish: async (on, metadata, event) => {
+      const { level, scope } = on;
       const { name, type } = event;
-      const channel = await connect();
+      const Channel = await connect();
 
       const ex = endpoints.toExchange({ level, scope, type });
       const topic = name;
       const text = JSON.stringify({ event, metadata });
       const content = Buffer.from(text);
-      await channel.assertExchange(ex, "direct", { autoDelete: true, durable: true });
-      await channel.publish(ex, topic, content);
-
-      return {
-        level,
-        name,
-        scope,
-        type,
-      };
+      await Channel.assertExchange(ex, "direct", { autoDelete: true, durable: true });
+      await Channel.publish(ex, topic, content);
     },
-    subscribe: async (on, to, process) => {
-      const channel = await connect();
-
-      const ex = endpoints.toExchange(on);
-      const q = endpoints.toQueue(to);
-      const topic = on.name;
-      await channel.assertExchange(ex, "direct", { autoDelete: true, durable: true });
-      await channel.assertQueue(q, { autoDelete: true, durable: true });
-      await channel.bindQueue(q, ex, topic);
-      const onMessage = channel.toOnMessageWithAck((data, ack) => {
-        const { event, metadata } = data;
-        const processAck = async () => {
-          ack();
-        };
-        process(event, metadata, processAck);
-      });
-
-      await channel.consume(q, onMessage, { noAck: false });
-
-      return {
-        on,
-        to,
-      };
+    subscribe: async (definition) => {
+      StreamDefinitions.push(definition);
     },
   };
 })(configs);
